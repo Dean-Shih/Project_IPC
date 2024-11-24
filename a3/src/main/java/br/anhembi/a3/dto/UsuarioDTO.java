06032b6e17bceb6bf5cf3be2c1d71a02b3d4c24f
Thiago Sardinha Correia <thi-correia@uol.com.br>
2024-11-24 15:35:45 -0300

    Adicionado usuario dto

---
+++ a3/src/main/java/br/anhembi/a3/dto/UsuarioDTO.java
@@ -0,0 +1,26 @@
+package br.anhembi.a3.dto;
+
+import br.anhembi.a3.model.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioDTO {
+    
+    @NotBlank(message = "O campo nome é obrigatório")
+    private String nome;
+
+    private String documento;
+    private String telefone;
+    private String email;
+    
+    @NotBlank(message = "O campo senha é obrigatório")
+    private String senha;
+    private int score;
+
+    public Usuario toUsuario(){
+        return new Usuario(this.nome, this.documento, this.telefone, this.email, this.senha, this.score);
+    }
+}
